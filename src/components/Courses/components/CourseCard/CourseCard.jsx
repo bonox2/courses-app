ced4a44815f958e7cba9c2f02771ec9bed89ae46
bonox2 <illiagoncharov99@gmail.com>
2022-06-18 20:15:16 -0400

    CourseCard component done

--- src/components/Courses/components/CourseCard/CourseCard.jsx
+++ src/components/Courses/components/CourseCard/CourseCard.jsx
@@ -6,14 +6,12 @@ export default function CourseCard(){
         <div className="course_card">
             <div className="course_info">
                 <h2 className="course_name">Java</h2>
-                <p className="course_description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatum dolores quibusdam aut nostrum itaque magni velit, aspernatur deserunt ut ex dolorem adipisci corrupti consequatur odit obcaecati eum dolore ullam facere temporibus rerum possimus molestias tempora? Aliquam earum esse odit fugit enim officiis, soluta voluptate ipsa, dignissimos reprehenderit iste cupiditate?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatum dolores quibusdam aut nostrum itaque magni velit, aspernatur deserunt ut ex dolorem adipisci corrupti consequatur odit obcaecati eum dolore ullam facere temporibus rerum possimus molestias tempora?</p>
             </div>
             <div className="course_details">
-                <div className="course_points">
-                    <div className="parameter">Authors:</div>
-                    <div className="parameter">Duration:</div>
-                    <div className="parameter">Created:</div>
-                </div>
+                    <div className="parameter">Authors: <div className="parameter_info"> Dave Simons</div></div>
+                    <div className="parameter">Duration: <div className="parameter_info"> 08:00 hours</div></div>
+                    <div className="parameter">Created: <div className="parameter_info"> 01.02.2018</div></div>
                 <button className="btn">Show course</button>
             </div>
         </div>
