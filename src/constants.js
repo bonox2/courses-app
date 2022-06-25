098f17f2b8cf5a1d721f64f1908623b66df9388b
bonox2 <illiagoncharov99@gmail.com>
2022-06-24 20:17:41 -0400

    authors overflow done

--- src/constants.js
+++ src/constants.js
@@ -26,7 +26,7 @@ export const mockedCoursesList = [
    specimen book.`,
         creationDate: '10/11/2020',
         duration: 210,
-        authors: ['df32994e-b23d-497c-9e4d-84e4dc02882f', '095a1817-d45b-4ed7-9cf7-b2417bcbf748'],
+        authors: ['df32994e-b23d-497c-9e4d-84e4dc02882f', '095a1817-d45b-4ed7-9cf7-b2417bcbf748','df32994e-b23d-497c-9e4d-84e4dc02882f', '095a1817-d45b-4ed7-9cf7-b2417bcbf748'],
     },
 ]
 export const mockedAuthorsList = [
