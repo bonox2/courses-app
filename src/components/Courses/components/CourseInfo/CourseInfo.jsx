b96e6031d937a994e921e66cfa5ec79be2594e77
bonox2 <illiagoncharov99@gmail.com>
2022-10-04 20:27:42 -0400

    CourseInfo css done

--- src/components/Courses/components/CourseInfo/CourseInfo.jsx
+++ src/components/Courses/components/CourseInfo/CourseInfo.jsx
@@ -1,6 +1,9 @@
 import { useParams } from 'react-router-dom';
 import Button from '../../../../common/Button/Button';
 import { mockedAuthorsList } from "../../../../constants";
+import './CourseInfo.css';
+import { Link } from 'react-router-dom';
+
 
 
 export default function CourseInfo({ courses }) {
@@ -14,15 +17,21 @@ export default function CourseInfo({ courses }) {
   ).join(", ");
   return (
   <>
-      <Button buttonText="Back to courses" className="course_info_btn"></Button>
-      <h2>{courseInfo.title}</h2>
-      <p>{courseInfo.description}</p>
-      <ul>
-        <li><span>ID: </span>{courseInfo.id}</li>
-        <li><span>Duration: </span>{courseInfo.duration}</li>
-        <li><span>Created: </span>{courseInfo.creationDate}</li>
-        <li><span>Authors: </span>{authorsNames}</li>
-      </ul>
+      <section className="course_info_main">
+        <div className="container">
+          <Link to="/courses" className="course_info_btn">Back to courses</Link>
+          <h2 className='course_info_title'>{courseInfo.title}</h2>
+          <div className="course_info_description_main">
+            <p className='course_info_text'>{courseInfo.description}</p>
+            <ul className='course_info_list'>
+              <li><span>ID</span>: {courseInfo.id}</li>
+              <li><span>Duration</span>: {courseInfo.duration}</li>
+              <li><span>Created</span>: {courseInfo.creationDate}</li>
+              <li><span>Authors</span>: {authorsNames}</li>
+            </ul>
+          </div>
+        </div>
+      </section>
   </>
   )
 }
